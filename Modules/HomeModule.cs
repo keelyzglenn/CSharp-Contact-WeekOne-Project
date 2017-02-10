2691298536be2e754ca9d223c626fb78acf84e5c
keely glenn <glenj392@newschool.edu>
2017-02-10 08:46:07 -0800

    add form for new contact to gather name address and number, then adds paths to post that information on the index page

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,14 +9,22 @@ namespace Contacts
   {
     public HomeModule()
     {
+// displays index with all contacts including new ones
       Get["/"] = _ => {
         List<Contact> allContacts = Contact.GetAll();
         return View["index.cshtml", allContacts];
       };
-
+// displays the new contact page
       Get["/contact/new"] = _ => {
         return View["new_contact.cshtml"];
       };
+// posts the new contact information on the index page
+      Post["/"] = _ =>{
+        Contact newContact = new Contact(Request.Form["new-name"], Request.Form["new-address"], Request.Form["new-number"]);
+        List<Contact> allContacts = Contacts.GetAll();
+        return View["index.cshtml", allContacts];
+      };
+
 // uses form button to clear all contacts
       Post["/tamagotchis/cleared"] = _ => {
       Contact.ClearAll();
