d0b9dc157a2f15586ac046659f4080f752e83d9e
keely glenn <glenj392@newschool.edu>
2017-02-10 08:37:08 -0800

    add clear pathway as well as cshtml page that confirms deleted contacts. Creates a form button on index to delete contacts. Adds a pathway to view new contact page- no content yet

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,6 +9,19 @@ namespace Contacts
   {
     public HomeModule()
     {
+      Get["/"] = _ => {
+        List<Contact> allContacts = Contact.GetAll();
+        return View["index.cshtml", allContacts];
+      };
+
+      Get["/contact/new"] = _ => {
+        return View["new_contact.cshtml"];
+      };
+// uses form button to clear all contacts
+      Post["/tamagotchis/cleared"] = _ => {
+      Contact.ClearAll();
+      return View["contacts_cleared.cshtml"];
+    };
     }
   }
 }
